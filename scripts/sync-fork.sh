20ca37073a9a80d6bc378530565f7c20e68cefc6
Olivier ORAND <olivier.orand@orange.com>
2016-06-08 12:16:08 +0200

    add progress info and fetch in quiet mode

--- scripts/sync-fork.sh
+++ scripts/sync-fork.sh
@@ -14,17 +14,21 @@ FINAL_RELEASE_REPO=target-sync
 git clone target ${FINAL_RELEASE_REPO}
 
 pushd upstream
-    git fetch --all
+    echo "Process upstream"
+    git fetch --all -q
     export UPSTREAM_URL=$(git ls-remote --get-url origin)
 popd
 
 pushd target
-    git fetch --all
+    echo "Process target"
+    git fetch --all -q
     export TARGET_URL=$(git ls-remote --get-url origin)
 popd
 
 
 pushd ${FINAL_RELEASE_REPO}/
+    echo "Process ${FINAL_RELEASE_REPO}"
+
     git remote add upstream ../upstream
     remote=upstream ; for brname in `git branch -r | grep upstream | grep -v master | grep -v HEAD | sed -e 's/.*\///g'`; do git branch --track $brname  $remote/$brname ; done
 
